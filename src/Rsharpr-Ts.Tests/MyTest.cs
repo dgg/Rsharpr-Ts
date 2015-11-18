c81147cdc246191daaa33730338df41be7a90df9
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-11-18 18:57:03 +0100

    add "old" NUnit project to prevent "too many new things" syndrom3

---
+++ src/Rsharpr-Ts.Tests/MyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Rsharpr_Ts.Tests
+{
+	[TestFixture]
+	public class MyTest
+	{
+		[Test]
+		public void subject_scenario_outcome()
+		{
+			Assert.That(true, Is.False);
+		}
+	}
+}
