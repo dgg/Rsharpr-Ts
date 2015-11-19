d8c6356626c7f9bc360cbd1579009dad8d262146
Daniel Gonzalez Garcia <danielgonnet@gmail.com>
2015-11-19 07:51:15 +0100

    implement macro plumbing to test local deployment

---
+++ src/Rsharpr-Ts/TimeStamp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rsharpr_Ts
+{
+	public static class TimeStamp
+	{
+		private static readonly DateTimeOffset Epoch = new DateTimeOffset(new DateTime(1970, 1, 1), TimeSpan.Zero);
+
+		public static ulong Next()
+		{
+			TimeSpan diff = DateTimeOffset.UtcNow - Epoch;
+			return Convert.ToUInt64(Math.Floor(diff.TotalSeconds));
+		}
+	}
+}
---
+++ src/Rsharpr-Ts/TsMacroDef.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using JetBrains.DocumentModel;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Hotspots;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Macros;
+
+namespace Rsharpr_Ts
+{
+	[MacroDefinition("tstamp", LongDescription = "Generates a new time stamp in a numeric format.", ShortDescription = "New time stamp")]
+	public class TsMacroDef : SimpleMacroDefinition
+	{
+		public override string GetPlaceholder(IDocument document, IEnumerable<IMacroParameterValue> parameters)
+		{
+			return "TSTAMP";
+		}
+	}
+	[MacroImplementation(Definition = typeof(TsMacroDef))]
+	public class TsMacroImpl : SimpleMacroImplementation
+	{
+		public override HotspotItems GetLookupItems(IHotspotContext context)
+		{
+			return MacroUtil.SimpleEvaluateResult("my timestamp");
+		}
+	}
+}
